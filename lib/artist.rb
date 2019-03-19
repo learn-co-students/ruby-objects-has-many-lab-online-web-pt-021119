ba2623725c5551747e6be4a2fb97beb927811d4a
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-19 06:22:43 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+require 'pry'
+class Artist 
+  attr_accessor :name, :songs
+  
+  @@artist_songs = 0
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end 
+  
+  def add_song(song)
+    @songs << song 
+    song.artist = self 
+    @@artist_songs += 1
+  end 
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song 
+    song.artist = self 
+    @@artist_songs += 1 
+  end 
+  
+  def self.song_count
+    @@artist_songs
+  end 
+  
+end 
